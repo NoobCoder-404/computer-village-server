74c93dcf28fe2324daeca4f341f56715a775f399
Noob-H4cker <alsamiul123@gmail.com>
2022-11-26 12:37:00 +0600

    products api

--- index.js
+++ index.js
@@ -10,12 +10,29 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.rqwof3p.mongodb.net/?retryWrites=true&w=majority`;
-console.log(uri);
+// console.log(uri);
 const client = new MongoClient(uri, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     serverApi: ServerApiVersion.v1,
 });
+
+async function run() {
+    try {
+        const productCollection = client.db('computerVillage').collection('products');
+
+        app.get('/products', async (req, res) => {
+            const query = {};
+            const cursor = productCollection.find(query);
+            const products = await cursor.toArray();
+            res.send(products);
+        });
+    } finally {
+        // await client.close();
+    }
+}
+run().catch((error) => console.error(error.message));
+
 app.get('/', (req, res) => {
     res.send('Server Running');
 });

6e9d4393c4252aa079399439b7d8599fe32b9782
Noob-H4cker <alsamiul123@gmail.com>
2022-11-26 12:08:01 +0600

    mongodb connect

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
+/* eslint-disable no-unused-vars */
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 require('dotenv').config();
 
 const Port = process.env.Port || 5000;
@@ -7,7 +9,13 @@ const app = express();
 
 app.use(express.json());
 app.use(cors());
-
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.rqwof3p.mongodb.net/?retryWrites=true&w=majority`;
+console.log(uri);
+const client = new MongoClient(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    serverApi: ServerApiVersion.v1,
+});
 app.get('/', (req, res) => {
     res.send('Server Running');
 });
